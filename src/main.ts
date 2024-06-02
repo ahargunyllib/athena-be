d547b9f734204b801cfc3bcaf5475dcf5fb90334
ahargunyllib <billy.bpm03@gmail.com>
2024-06-02 21:33:44 +0700

    feat: helmet and cors for security

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,9 @@ async function bootstrap() {
   app.useLogger(loggerService);
 
   app.enableShutdownHooks();
+  app.enableCors();
+
+  app.use(helmet());
   
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT') || 3000);
